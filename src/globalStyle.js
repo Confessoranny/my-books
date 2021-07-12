1cac4376aabd69f10466c22b9fe1bae73ab784d1
Anny Confessor <anny.confessor@gmail.com>
2021-07-12 20:15:28 -0300

    Added global styles and reset css

--- src/globalStyle.js
+++ src/globalStyle.js
@@ -12,32 +12,15 @@ export const theme = {
     }
 }
 
-export const BaseStyles = createGlobalStyle`
+export const GlobalStyle = createGlobalStyle`
   ${reset}
 
   body, html {
     font-family: "Nunito", sans-serif;
     font-weight: 500;
     font-size: 14px !important;
-    padding: 0;
-  }
-
-  * {
-    box-sizing: border-box;
-  }
-
-  p {
-    margin-bottom: 0.75rem;
-  }
-
-  a {
-    text-decoration: unset;
-    color: unset;
-  }
-
-  .App {
-    min-height: 100vh;
-    display: flex;
-    flex-direction: column;
+    margin: 0;
   }
 `
+
+export default GlobalStyle
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 
-import Routes from './Routes'
+import Routes from './Routes';
+import GlobalStyle from './globalStyle';
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle whiteColor />
     <Routes />
   </React.StrictMode>,
   document.getElementById('root')
